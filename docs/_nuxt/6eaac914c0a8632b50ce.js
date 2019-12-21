918c63a1dc242813fc875a5358ce36e28cf38427
Alexei <alexeimyshkouski@gmail.com>
2019-12-21 20:02:32 +0300

    Initial layout

---
+++ docs/_nuxt/6eaac914c0a8632b50ce.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{141:function(n,t,e){"use strict";e.r(t);var l={data:function(){}},c=e(22),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("h1",[this._v("hello")])}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export default {
+    build: {
+        parallel: true,
+        // cache: true,
+        // optimizeCSS: true,
+        extractCSS: true
+    },
+
+    generate: {
+        dir: 'docs'
+    },
+
+    router: {
+        base: process.env.NODE_ENV == 'production' ? process.env.ROUTER_BASE || '/' : '/'
+    }
+}
